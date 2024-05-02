451927963903d981f6f45f2cc9a8d7daac2598ba
DamianAguero1337 <129700153+DamianAguero1337@users.noreply.github.com>
2024-05-01 23:24:33 -0300

    primer commit

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { NavBar } from "../components/ui/NavBar/NavBar"; // Importamos el componente NavBar
+
+// Componente AppRouter que define las rutas de la aplicación
+export const AppRouter = () => {
+  return (
+    <>
+      {/* Barra de navegación */}
+      <NavBar />
+      {/* Definición de las rutas */}
+      <Routes>
+        
+      </Routes>
+    </>
+  );
+};
